f9f556d178269b5892498d6a6b811dc414241b35
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-26 21:42:22 -0500

    player and bullet group

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,22 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 
 class PlayState extends FlxState
 {
+	public var player:FlxSprite;
+
+	public var bulletGroup:FlxTypedGroup<FlxSprite> = new FlxTypedGroup<FlxSprite>();
+
 	override public function create()
 	{
 		super.create();
+		player = new FlxSprite().makeGraphic(48, 48, 0xff0000);
+		add(player);
+
+		add(bulletGroup);
 	}
 
 	override public function update(elapsed:Float)
