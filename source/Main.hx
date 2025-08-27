25d14010e8d9b75652a97ea0c5676fa141307e32
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-27 15:52:07 -0500

    boom, thanks fnf, you helped me with sum stuff, AGAIN

--- source/Main.hx
+++ source/Main.hx
@@ -1,6 +1,7 @@
 package;
 
 import flixel.FlxGame;
+import modding.PolymodHandler;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -8,6 +9,8 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+
+		PolymodHandler.loadAllMods();
 
 		addChild(new FlxGame(0, 0, PlayState));
 	}
