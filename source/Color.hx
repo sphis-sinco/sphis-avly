374be662964f2d21ef809d010af6de5e85070ab1
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-26 21:47:34 -0500

    genuine question

--- source/Color.hx
+++ source/Color.hx
@@ -4,6 +4,7 @@ import flixel.util.FlxColor;
 
 class Color
 {
+	// why do I wanna make a cache system for commonly used colors?
 	public static function fstr(hexStr:String):FlxColor
 	{
 		return FlxColor.fromString(hexStr);
