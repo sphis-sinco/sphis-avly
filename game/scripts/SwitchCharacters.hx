13ef1ef47d01eb3dad59e224c91b552422e4d387
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-01 10:33:32 -0500

    ==?

--- game/scripts/SwitchCharacters.hx
+++ game/scripts/SwitchCharacters.hx
@@ -5,9 +5,9 @@ function onUpdate(elapsed:Float)
 	if (FlxG.keys.justReleased.R)
 	{
 		if (PlayState.player_character == Characters.NORMAL_DIFF)
-			PlayState.player_character == Characters.HARD_DIFF;
+			PlayState.player_character = Characters.HARD_DIFF;
 		else
-			PlayState.player_character == Characters.NORMAL_DIFF;
+			PlayState.player_character = Characters.NORMAL_DIFF;
 
 		FlxG.resetState();
 	}
