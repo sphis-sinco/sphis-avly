5a8a25ce8a6a1f7fe95df7d1a34a178ae84534fd
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-01 12:44:25 -0500

    workin on it

--- source/ModManager.hx
+++ source/ModManager.hx
@@ -1,8 +1,23 @@
 package;
 
+import sys.FileSystem;
+
 class ModManager
 {
-	public static var mods:Array<String> = [];
+	public static var MOD_IDS:Array<String> = [];
 
-	public static function loadMods() {}
+	public static var MODS_FOLDER:String = 'game/mods/';
+
+	public static function loadMods()
+	{
+		#if sys
+		for (entry in FileSystem.readDirectory(MODS_FOLDER))
+		{
+			if (FileSystem.isDirectory(entry))
+			{
+				for (file in FileSystem.readDirectory('$MODS_FOLDER/$entry')) {}
+			}
+		}
+		#end
+	}
 }
